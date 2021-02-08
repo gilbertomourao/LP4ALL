0e017ca520bb19f246b026cd4313ba7d9632f5b0
Gilberto Mourão <gilbertojos.mourao@gmail.com>
2021-02-08 18:47:00 -0300

    add P3 com linp

---
+++ 2020.1/P3/q1p3_com_linp.c
@@ -0,0 +1,110 @@
+/**
+ * Solução da questão 1 da prova 3 com LINP
+ */
+
+#include <linp.h>
+#include <string.h> /* Para strlen() */
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+void invstr(char *);
+void substitui(Linp_Mat *, char *);
+
+int main()
+{
+	Linp_Mat *str1 = lp.criarmat(1, MAX_SIZE);
+	Linp_Mat *dst = lp.criarmat(1, MAX_SIZE);
+	char str2[MAX_SIZE];
+
+	lp.input("String 1: ", "%s", str1->data[0], MAX_SIZE);
+	lp.input("String 2: ", "%s", str2, MAX_SIZE);
+
+	/**
+	 * Antes de usar a função procstr, o valor de cols 
+	 * da Linp_Mat str1 deve ser corrigido para o tamanho 
+	 * da string lida. Alterar o membro cols de Linp_Mat 
+	 * não afeta a função destruirmat.
+	 */
+	unsigned i;
+	for (i = 0; str1->data[0][i] != '\0'; i++);
+	str1->cols = i;
+	dst->cols = i;
+
+	/* Procura str2 em str1 sem ignorar qualquer 
+	 * caractere e sem ignorar case sensitive. 
+	 * dst irá armazenar str1 com as ocorrências de 
+	 * str2 destacadas.
+	 */
+	lp.procstr(str1, dst, NULL, str2, "linhas", "", false);
+
+	lp.dispmat(dst, "Ocorrencias: ");
+
+	invstr(str2);
+	substitui(dst, str2);
+
+	lp.dispmat(dst, "Resposta: ");
+
+	/**
+	 * Se quiser imprimir do modo convencional, 
+	 * utilizando a função printf, declare um ponteiro 
+	 * para um char e aponte para dst. 
+	 *
+	 * Antes, insira um '\0' na última posição de dst.
+	 */
+	dst->data[0][dst->cols] = '\0';
+	char *ptr = dst->data[0];
+	printf("String resposta: %s\n", ptr);
+
+	/* Libera a memória previamente alocada */
+	lp.destruirmat(str1);
+	lp.destruirmat(dst);
+
+	return 0;
+}
+
+/**
+ * Função que inverte a string str passada como
+ * parâmetro.
+ */
+void invstr(char *str)
+{
+	char aux;
+	unsigned i;
+	unsigned j = strlen(str);
+
+	for (i = 0, j = j - 1; i < j; i++, j--)
+	{
+		aux = str[i];
+		str[i] = str[j];
+		str[j] = aux;
+	}
+}
+
+/**
+ * Função que substitui as ocorrências pela 
+ * string invertida
+ */
+void substitui(Linp_Mat *str1, char *strinv)
+{
+	unsigned i, j;
+
+	for (i = 0; i < str1->cols; i++)
+	{
+		if (str1->data[0][i] != (char) 250)
+		{
+			for (j = 0; strinv[j]; j++, i++)
+			{
+				str1->data[0][i] = strinv[j];
+			}
+			--i; /* Ajusta o valor de i */
+		}
+	}
+
+	/* O professor pediu para substituir por '*', nesse caso... */
+	for (i = 0; i < str1->cols; i++)
+	{
+		if (str1->data[0][i] == (char) 250)
+			str1->data[0][i] = '*';
+	}
+}
---
+++ 2020.1/P3/q2p3_com_linp.c
@@ -0,0 +1,38 @@
+/**
+ * Solução da questão 2 da prova 3 com LINP
+ */
+
+#include <linp.h>
+
+void transforma(Linp_Mat *);
+
+int main()
+{
+	Linp_Mat *mat = lp.criarmat(100, 100);
+
+	lp.lerarquivo(mat, "matriz.txt");
+
+	lp.dispmat(mat, "Matriz Original");
+	transforma(mat);
+	lp.dispmat(mat, "Matriz Transformada");
+
+	lp.destruirmat(mat);
+
+	return 0;
+}
+
+void transforma(Linp_Mat *mat)
+{
+	unsigned i, j;
+	for (i = 0; i < mat->rows; i++)
+	{
+		for (j = 0; j < mat->cols; j++)
+		{
+			if (mat->data[i][j] >= 'A' && mat->data[i][j] <= 'Z')
+				mat->data[i][j] += 'a' - 'A';
+			else 
+				if (mat->data[i][j] >= 'a' && mat->data[i][j] <= 'z')
+					mat->data[i][j] += 'A' - 'a';
+		}
+	}
+}
