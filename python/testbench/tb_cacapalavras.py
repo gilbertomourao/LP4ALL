d4b13a4bc8b4fbb734d45ff42ee8968a3094625d
gilbertomourao <gilbertojos.mourao@gmail.com>
2022-06-01 22:27:47 -0300

    added comments

--- python/testbench/tb_cacapalavras.py
+++ python/testbench/tb_cacapalavras.py
@@ -2,9 +2,11 @@ import pylinp as pl
 
 #print(pl.cacapalavras.__doc__)
 
+# Arquivos texto
 src_file = 'desafio.txt'
 dst_file = 'gabarito.txt'
 
+# Lista de palavras
 L = ['Aterramento',
 'SPDA',
 'Protecao',
@@ -16,6 +18,15 @@ L = ['Aterramento',
 'Residencial',
 'Predial']
 
+"""
+Procura por todas as palavras em L dentro de src. 
+Destaca as palavras encontradas em dst. 
+Procura nas linhas, colunas, diagonais no sentido 
+da principal e no sentido da secundária (LCPS), 
+nas duas direções. 
+Não ignora nenhum caractere. 
+Ignora case sensitive. 
+"""
 pl.cacapalavras(src = src_file, 
 	dst = dst_file, 
 	palavras = L, 
